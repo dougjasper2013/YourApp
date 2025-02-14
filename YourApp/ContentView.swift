749f0c9881e3bc8e4087803eccbba97221464a04
Douglas Jasper <doug.jasper@trios.com>
2025-02-14 08:33:54 -0500

    Initial Commit

---
+++ YourApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YourApp
+//
+//  Created by Douglas Jasper on 2025-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YourApp/YourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourAppApp.swift
+//  YourApp
+//
+//  Created by Douglas Jasper on 2025-02-14.
+//
+
+import SwiftUI
+
+@main
+struct YourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
